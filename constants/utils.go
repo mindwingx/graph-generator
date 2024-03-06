5183fae714c91cb4def6422443d1f7f35a22f01e
Milad <myzerone2@gmail.com>
2024-03-06 09:55:35 +0100

    [branch][master] items fixed for docker network

--- constants/utils.go
+++ constants/utils.go
@@ -1,6 +1,7 @@
 package constants
 
 const (
-	TmpLockFile   = "/tmp/log-app.lock"
-	AggregatorUrl = "http://localhost:8880/api/send"
+	TmpLockFile = "/tmp/log-app.lock"
+	//AggregatorUrl = "http://localhost:8880/api/send"
+	AggregatorUrl = "http://coordinator:8880/api/send" // docker network
 )
